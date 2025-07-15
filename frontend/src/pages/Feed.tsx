1e4a39459d9b75ae4ecc39d5850244a90d31b30f
AbhishekDhallania <dhallaniaabhishek@gmail.com>
2025-07-16 00:59:50 +0530

    Right Side Bar Done

--- frontend/src/pages/Feed.tsx
+++ frontend/src/pages/Feed.tsx
@@ -1,6 +1,10 @@
+import { AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import useTheme from "@/hooks/useTheme";
-import { Activity, Bell, Ellipsis, HomeIcon, Mail, SearchIcon, SunMoon, User, Users, Zap } from "lucide-react";
+import { Avatar } from "@radix-ui/react-avatar";
+import { Activity, Bell, Ellipsis, HomeIcon, Mail, Search, SearchIcon, SunMoon, User, Users, Zap } from "lucide-react";
 
 const menu = [
     {
@@ -55,6 +59,24 @@ const menu = [
     }
 ]
 
+const trends = [
+    { category: "Trending in India", topic: "#ReactJS", posts: "12.3K" },
+    { category: "Trending in Tech", topic: "JavaScript", posts: "9.1K" },
+    { category: "Trending Worldwide", topic: "OpenAI", posts: "20.4K" },
+];
+
+const suggestions = [
+    {
+        name: "Abhishek",
+        username: "@abhishek",
+        image: "/K_Logo.jpg"
+    },
+    {
+        name: "Kapil Kumar",
+        username: "@kk",
+        image: "/K_Logo.jpg",
+    },
+];
 
 function Home() {
     const { theme, toggleTheme } = useTheme();
@@ -93,9 +115,83 @@ function Home() {
                 </div>
             </div>
             <div className=" flex-1/2">center</div>
-            <div className=" hidden md:block flex-1/4 border-l-1">rightsidebar</div>
+
+            {/* rightSidebar */}
+
+            {/* 1.Top */}
+            <div className="hidden md:block flex-1/4 border-l-1 ">
+                <div className="mx-8 mt-2 relative w-full max-w-md ">
+                    <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={18} />
+                    <Input
+                        type="text"
+                        placeholder="Search"
+                        className="rounded-3xl pl-10 w-96 text-xl"
+                    />
+                </div>
+                {/* Whatshappening */}
+                <div className="" >
+                    <Card className="mx-8 mt-4 gap-1.5">
+                        <CardContent className="px-4 space-y-3">
+                            <h2 className="font-bold text-lg">What’s happening</h2>
+                            {trends.map((item, idx) => (
+                                <div key={idx} className="flex flex-col text-sm hover:bg-primary/10 w-full hover:cursor-pointer">
+                                    <p className="text-gray-500 flex justify-between ">{item.category}
+                                        <Ellipsis className="hover:bg-sky-100 rounded-full color:sky:100" />
+                                    </p>
+                                    <p className="font-bold">{item.topic}</p>
+                                    <p className="text-gray-500 text-xs">{item.posts} Posts</p>
+                                </div>
+                            ))}
+                            <p className="text-blue-400 hover:bg-primary/10 hover:cursor-pointer">Show more</p>
+                        </CardContent>
+                    </Card>
+                </div>
+
+                {/* Who to follow */}
+
+                <div className="mx-8 mt-2">
+                    <Card>
+                        <CardContent className=" space-y-3">
+                            <h2 className="font-bold text-lg">Who to follow</h2>
+                            <div className="hover:cursor-pointer">
+                                {suggestions.map((user, idx) => (
+                                    <div key={idx} className="flex items-center justify-between hover:bg-primary/10">
+                                        <div className="flex items-center gap-3 ">
+                                            <Avatar className="w-10 h-10">
+                                                <AvatarImage src={user.image} alt={user.name} className="rounded-full" />
+                                                <AvatarFallback className="hover:underline">{user.name[0]}</AvatarFallback>
+                                            </Avatar>
+                                            <div>
+                                                <p className="font-medium text-sm">{user.name}</p>
+                                                <p className="text-gray-500 text-xs">{user.username}</p>
+                                            </div>
+                                        </div>
+                                        <Button className="rounded-full">
+                                            Follow
+                                        </Button>
+                                    </div>
+                                ))}
+                            </div>
+                            <p className="text-blue-400 hover:bg-primary/10 ">Show more</p>
+                        </CardContent>
+                    </Card>
+                </div>
+
+                {/* footer  */}
+                <div className="text-xs text-gray-500 space-x-2 mx-8 mt-4">
+                    <span className="hover:underline cursor-pointer">Terms of Service    |</span>
+                    <span className="hover:underline cursor-pointer">Privacy Policy    |</span>
+                    <span className="hover:underline cursor-pointer">Cookie Policy    |</span>
+                    <div></div>
+                    <span className="hover:underline">More  ...</span>
+                    <span>© 2025 Y Corp.</span>
+
+                </div>
+
+            </div>
         </div>
     );
 }
+
 
 export default Home
